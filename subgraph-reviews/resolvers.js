5ce0b1cfd2f00971e9039e0ed41d5a93761614ab
y0ng0p3 <vaillantyomen@gmail.com>
2023-02-16 13:16:50 +0100

    add location field to reviews.Review type. add Review resolver reviews subgraph add Location resolver and its __resolveReference in locations subgraph

--- subgraph-reviews/resolvers.js
+++ subgraph-reviews/resolvers.js
@@ -2,14 +2,19 @@ const resolvers = {
   Query: {
     latestReviews: (_, __, {dataSources}) => {
       return dataSources.reviewsAPI.getLatestReviews();
-    }
+    },
   },
   Mutation: {
     submitReview: (_, {locationReview}, {dataSources}) => {
       const newReview = dataSources.reviewsAPI.submitReviewForLocation(locationReview);
       return {code: 200, success: true, message: 'success', locationReview: newReview};
-    }
-  }
+    },
+  },
+  Review: {
+    location: ({locationId}) => {
+      return {id: locationId};
+    },
+  },
 };
 
 module.exports = resolvers;
